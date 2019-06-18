18bfe3bcc124354b2b541c172e33c164c5c2aa74
lilian <lilian@lod-wansch@hotmail.fr>
2019-06-18 17:01:51 +0200

    création dossier et fichiers et co bdd

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="/css/index.css">
   </head>
   <body>
     <?php include "header.php"; ?>
---
+++ php/connex_bdd.php
@@ -0,0 +1,12 @@
+<?php
+try
+{
+    // On se connecte à MySQL
+$bdd = new PDO('mysql:host=localhost;dbname=ma-bdd;charset=utf8', 'user', 'mdp', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(PDOexception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        print 'Erreur : '.$e->getMessage(). '<br />';
+        die();
+}
