effc73dad71c8790918c36ff62889e03fea72c31
Linkels <m.chaumoitre@simplon-charleville.fr>
2019-06-18 17:07:01 +0200

    main css

--- accueil.php
+++ accueil.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
-    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
 
