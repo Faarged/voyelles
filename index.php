b5cabf1c60c0a5b74a4fa5a33917aff2201498c4
lilian <lilian@lod-wansch@hotmail.fr>
2019-06-20 13:21:54 +0200

    mise en page formulaire index

--- index.php
+++ index.php
@@ -4,14 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
   </head>
   <body>
 
 
 <div class="formulaire_co">
   <form class="formulaire_co" method="POST" action="php/connect.php">
+    <label>Entrez votre pseudo ou n° de carte:</label>
     <input type="text" id="pseudo1" name="pseudo1" placeholder="Pseudo" />
+    <label>Entrez votre mot de passe:</label>
     <input type="password" id="pass1" name="pass1" placeholder="Mdp" />
     <button>Connexion</button>
   </form>
