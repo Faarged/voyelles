975ff4e3e775f35a9d2cad9b79348062dd9d4ef0
lilian <lilian@lod-wansch@hotmail.fr>
2019-06-20 15:26:39 +0200

    prépare la co à la bdd "consonne"

--- php/connex_bdd.php
+++ php/connex_bdd.php
@@ -2,7 +2,7 @@
 try
 {
     // On se connecte à MySQL
-$bdd = new PDO('mysql:host=localhost;dbname=ma-bdd;charset=utf8', 'user', 'mdp', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd = new PDO('mysql:host=localhost;dbname=consonne;charset=utf8', 'user', 'mdp', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch(PDOexception $e)
 {
