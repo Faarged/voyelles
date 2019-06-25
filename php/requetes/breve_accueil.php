cdecfef6f0bb4ecb281f6ae0bc9ba02a2cc32fb0
lilian <l.stoklosa@laposte.net>
2019-06-25 16:54:14 +0200

    page accueil fonctionnelle

--- php/requetes/breve_accueil.php
+++ php/requetes/breve_accueil.php
@@ -1,5 +1,5 @@
 <?php
-  require '../connex_bdd.php';
+  require "php/connex_bdd.php";
   $reponse = $bdd->prepare("SELECT * FROM breves ORDER BY id_breves DESC LIMIT 1");
   $reponse->execute();
 
--- php/requetes/requete_user.php
+++ php/requetes/requete_user.php
@@ -1,5 +1,5 @@
 <?php
-  require '../connex_bdd.php';
+  require "php/connex_bdd.php";
   $reponse = $bdd->prepare("SELECT * FROM users LIMIT 5");
   $reponse->execute();
 ?>
