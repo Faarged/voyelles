3b639293f18c41f83bb9000d7633475a793529b1
lilian <l.stoklosa@laposte.net>
2019-06-27 10:45:37 +0200

    Complétion partielle navadmin

--- navadmin.php
+++ navadmin.php
@@ -1,10 +1,12 @@
 <nav>
   <img src="img/calen.png" alt="">
-  <ui>
-    <li>lien 1</li>
-    <li>lien 2</li>
-    <li>lien 3</li>
-    <li>lien 4</li>
-    <li>lien 5</li>
-    <ul>
+  <h3>Administration</h3>
+  <ul>
+    <li><a href="liste_adherents.php">Adhérents</a></li>
+    <li><a href="liste_admin.php">Administrateurs</a></li>
+    <li><a href="#">Brèves</a></li>
+    <li><a href="#">Matériel</a></li>
+    <li><a href="#">Configuration</a></li>
+    <li><a href="#">Statistiques</a></li>
+  <ul>
 </nav>
