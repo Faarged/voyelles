b9ba66e7e1de522f5146207ed415c3a93d3d791c
Linkels <m.chaumoitre@simplon-charleville.fr>
2019-06-28 10:44:42 +0200

    nav 3

---
+++ navadmin.php
@@ -0,0 +1,14 @@
+<div class="navadmin">
+  <nav>
+    <img src="img/calen.png" alt="">
+    <h3>Administration</h3>
+    <ul>
+      <li><a href="liste_adherents.php">Adhérents</a></li>
+      <li><a href="liste_admin.php">Administrateurs</a></li>
+      <li><a href="breves.php">Brèves</a></li>
+      <li><a href="#">Matériel</a></li>
+      <li><a href="#">Configuration</a></li>
+      <li><a href="#">Statistiques</a></li>
+    <ul>
+  </nav>
+</div>
