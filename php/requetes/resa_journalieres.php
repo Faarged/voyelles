d0e1d21ba567835e1a00e7c0c9c0c399a5dbef9d
lilian <l.stoklosa@laposte.net>
2019-06-28 09:29:22 +0200

    corrige et rend fonctionnelle req resa et resa journaliere

--- php/requetes/resa_journalieres.php
+++ php/requetes/resa_journalieres.php
@@ -1,9 +1,11 @@
 <?php
   require "php/connex_bdd.php";
+  $date = date("Y-m-d");
   $reponse = $bdd->prepare("SELECT * FROM reservation, users, fait
     WHERE reservation.id_resa = fait.id_resa
     AND users.id_user = fait.id_user
-    AND users.id_user ='".$_SESSION['id'].
-    "'LIMIT 10");
-  $reponse->execute();
+    AND reservation.date_resa = :date_jour");
+  $reponse->execute(array(
+    'date_jour' => $date
+  ));
 ?>
--- reservations.php
+++ reservations.php
@@ -62,6 +62,25 @@ if ($_SESSION['statut'] == 'adherent')
             <input type="text" name="materiel1" placeholder="Matériel">
           <button type="submit" name="submit" class="valider" value="envoyer">Valider</button>
         </form>
+        <table>
+          <tr>
+            <td>Utilisateur</td>
+            <td>Date de réservation</td>
+            <td>Heure de début</td>
+            <td>Temps de réservation</td>
+            <td>Matériel réservé</td>
+          </tr>
+          <?php include "php/requetes/resa_journalieres.php";
+          while ($donnees = $reponse->fetch()){ ?>
+          <tr>
+            <td><?php echo $donnees['nom']." ".$donnees['prenom']; ?></td>
+            <td><?php echo $donnees['date_resa']; ?></td>
+            <td><?php echo $donnees['debut_resa']; ?></td>
+            <td><?php echo $donnees['duree'] ?></td>
+            <td><?php echo $donnees['materiel_res'] ?></td>
+          </tr>
+          <?php } ?>
+        </table>
       </div>
       <?php include "footer.php"; ?>
     </body>
