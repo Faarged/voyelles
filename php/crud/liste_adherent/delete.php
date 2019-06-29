05ae24cc895bba7f17bcac84a9bf5e56df775b39
lilian <l.stoklosa@laposte.net>
2019-06-29 09:43:12 +0200

    delete fonctionnel

--- php/crud/liste_adherent/delete.php
+++ php/crud/liste_adherent/delete.php
@@ -6,10 +6,13 @@ include("../config.php");
 $id = $_GET['id'];
 
 //deleting the row from table
+$jointure = "DELETE FROM fait WHERE id_user=:id";
+$supprime = $dbConn->prepare($jointure);
+$supprime->execute(array(':id' => $id));
 $sql = "DELETE FROM users WHERE id_user=:id";
 $query = $dbConn->prepare($sql);
 $query->execute(array(':id' => $id));
 
 //redirecting to the display page (index.php in our case)
-header("Location: ../../../index.php");
+header("Location: ../../../liste_adherents.php");
 ?>
