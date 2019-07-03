4fe8deef92fe6b42ffc3cc5933d9d370d036ebea
lilian <l.stoklosa@laposte.net>
2019-07-03 10:44:31 +0200

    amélio  requete user page accueil

--- php/requetes/requete_user.php
+++ php/requetes/requete_user.php
@@ -4,6 +4,8 @@
     WHERE users.id_user = fait.id_user
     AND reservation.id_resa = fait.id_resa
     AND date_resa = CURDATE()
+    AND CURTIME() >= ADDTIME(\"reservation.debut_resa\", \"reservation.duree\")
+    ORDER BY reservation.debut_resa
     LIMIT 5");
   $reponse->execute();
 ?>
