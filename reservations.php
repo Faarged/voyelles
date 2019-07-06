8bffe72660ce978dd533a0a2bc8e685503ba7d0d
lilian <l.stoklosa@laposte.net>
2019-07-06 08:51:57 +0200

    bloque la durée max a 2H

--- reservations.php
+++ reservations.php
@@ -59,7 +59,7 @@ if ($_SESSION['statut'] == 'adherent')
             <label for="debut_resa1">Début de la réservation</label>
               <input type="time" name="debut_resa1" min='10:00' max='17:45' value="<?php echo date('H:i'); ?>">
             <label for="duree1">Temps de réservation</label>
-              <input type="time" name="duree1" step='300'>
+              <input type="time" name="duree1" max='02:00' step='300'>
             <label for="materiel1">Matériel réservé</label>
               <input type="text" name="materiel1" placeholder="Matériel">
             <button type="submit" name="submit" class="valider" value="envoyer">Valider</button>
