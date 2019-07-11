1b8f8cc1b1cdabdd9fd8e5ceabbb60baa62b3693
lilian <l.stoklosa@laposte.net>
2019-07-11 14:02:28 +0200

    ajouts order by

--- php/requetes/jeu_list.php
+++ php/requetes/jeu_list.php
@@ -2,6 +2,7 @@
   require "php/connex_bdd.php";
   $reponse = $bdd->prepare('SELECT * FROM jeux, materiel, disponible
       WHERE jeux.id_jeu = disponible.id_jeu
-      AND materiel.id_materiel = disponible.id_materiel');
+      AND materiel.id_materiel = disponible.id_materiel
+      ORDER BY pegi_jeu DESC');
   $reponse->execute();
 ?>
