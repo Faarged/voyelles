7428306b1d2f5d0c384bb8b62e30ae8648c1044c
lilian <l.stoklosa@laposte.net>
2019-07-12 09:34:23 +0200

    nouvelle bdd

--- consonne.sql
+++ consonne.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Jeu 11 Juillet 2019 à 10:15
+-- Généré le :  Ven 12 Juillet 2019 à 09:33
 -- Version du serveur :  5.7.26-0ubuntu0.18.04.1
 -- Version de PHP :  7.2.19-0ubuntu0.18.04.1
 
@@ -37,8 +37,6 @@ CREATE TABLE `breves` (
 --
 
 INSERT INTO `breves` (`id_breves`, `titre_breves`, `contenu_breves`) VALUES
-(10, 'Stats', 'Réfléchir à comment faire la page des stats'),
-(11, 'stats pegi', 'Faire fonction sql qui compte le nb de chaq pegi inscrit et le mettre sous forme de barres'),
 (12, 'tableau jeu', 'Voir affichage des supports de jeu ');
 
 -- --------------------------------------------------------
@@ -107,7 +105,6 @@ CREATE TABLE `fait` (
 
 INSERT INTO `fait` (`id_resa`, `id_user`) VALUES
 (5, 2),
-(6, 2),
 (7, 2),
 (11, 2),
 (12, 2),
@@ -122,6 +119,7 @@ INSERT INTO `fait` (`id_resa`, `id_user`) VALUES
 (16, 6),
 (23, 6),
 (26, 6),
+(30, 6),
 (17, 7),
 (18, 7),
 (19, 7),
@@ -174,7 +172,9 @@ INSERT INTO `materiel` (`id_materiel`, `nom_materiel`, `type`) VALUES
 (5, 'Xbox One', 'console'),
 (6, 'Xbox One', 'accessoire'),
 (7, 'Wii U', 'console'),
-(8, 'Wii U', 'accessoire');
+(8, 'Wii U', 'accessoire'),
+(9, 'poste 1', 'pc'),
+(10, 'poste 2', 'pc');
 
 -- --------------------------------------------------------
 
@@ -187,37 +187,40 @@ CREATE TABLE `reservation` (
   `date_resa` date NOT NULL COMMENT 'format AAAA-JJ-MM',
   `debut_resa` time NOT NULL,
   `duree` time NOT NULL,
-  `materiel_res` varchar(50) NOT NULL
+  `materiel_res` varchar(50) NOT NULL,
+  `jeu_reserv` varchar(70) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `reservation`
 --
 
-INSERT INTO `reservation` (`id_resa`, `date_resa`, `debut_resa`, `duree`, `materiel_res`) VALUES
-(4, '2019-12-05', '05:35:00', '01:00:00', 'Xbox one'),
-(5, '2019-06-30', '13:00:00', '00:30:00', 'Switch'),
-(6, '2019-06-28', '13:00:00', '01:00:00', 'Switch'),
-(7, '2019-06-28', '14:00:00', '01:00:00', 'Switch'),
-(11, '2019-07-03', '11:00:00', '01:00:00', 'Switch'),
-(12, '2019-07-03', '13:00:00', '01:30:00', 'Xbox one'),
-(13, '2019-07-03', '10:15:00', '00:30:00', 'Xbox one'),
-(14, '2019-07-03', '14:15:00', '01:00:00', 'Switch'),
-(15, '2019-07-03', '17:15:00', '00:30:00', 'ps4'),
-(16, '2019-07-03', '11:00:00', '00:15:00', 'ps4'),
-(17, '2019-07-03', '14:15:00', '00:15:00', 'Switch'),
-(18, '2019-07-03', '13:00:00', '00:30:00', 'Xbox one'),
-(19, '2019-07-03', '11:30:00', '00:30:00', 'Xbox one'),
-(20, '2019-07-05', '10:45:00', '00:15:00', 'Xbox one'),
-(21, '2019-07-05', '13:30:00', '00:30:00', 'ps4'),
-(22, '2019-07-06', '11:45:00', '00:15:00', 'Xbox one'),
-(23, '2019-07-06', '12:55:00', '00:30:00', 'Switch'),
-(24, '2019-07-06', '15:37:00', '00:30:00', 'Switch'),
-(25, '2019-07-06', '15:38:00', '01:00:00', 'Switch'),
-(26, '2019-07-06', '15:38:00', '00:30:00', 'Xbox one'),
-(27, '2019-07-06', '16:46:00', '00:20:00', 'Switch'),
-(28, '2019-07-09', '10:00:00', '00:30:00', 'Switch'),
-(29, '2019-07-09', '15:41:00', '01:00:00', 'Xbox one');
+INSERT INTO `reservation` (`id_resa`, `date_resa`, `debut_resa`, `duree`, `materiel_res`, `jeu_reserv`) VALUES
+(4, '2019-12-05', '05:35:00', '01:00:00', 'Xbox one', NULL),
+(5, '2019-06-30', '13:00:00', '00:30:00', 'Switch', NULL),
+(6, '2019-06-28', '13:00:00', '01:00:00', 'Switch', NULL),
+(7, '2019-06-28', '14:00:00', '01:00:00', 'Switch', NULL),
+(11, '2019-07-03', '11:00:00', '01:00:00', 'Switch', NULL),
+(12, '2019-07-03', '13:00:00', '01:30:00', 'Xbox one', NULL),
+(13, '2019-07-03', '10:15:00', '00:30:00', 'Xbox one', NULL),
+(14, '2019-07-03', '14:15:00', '01:00:00', 'Switch', NULL),
+(15, '2019-07-03', '17:15:00', '00:30:00', 'ps4', NULL),
+(16, '2019-07-03', '11:00:00', '00:15:00', 'ps4', NULL),
+(17, '2019-07-03', '14:15:00', '00:15:00', 'Switch', NULL),
+(18, '2019-07-03', '13:00:00', '00:30:00', 'Xbox one', NULL),
+(19, '2019-07-03', '11:30:00', '00:30:00', 'Xbox one', NULL),
+(20, '2019-07-05', '10:45:00', '00:15:00', 'Xbox one', NULL),
+(21, '2019-07-05', '13:30:00', '00:30:00', 'ps4', NULL),
+(22, '2019-07-06', '11:45:00', '00:15:00', 'Xbox one', NULL),
+(23, '2019-07-06', '12:55:00', '00:30:00', 'Switch', NULL),
+(24, '2019-07-06', '15:37:00', '00:30:00', 'Switch', NULL),
+(25, '2019-07-06', '15:38:00', '01:00:00', 'Switch', NULL),
+(26, '2019-07-06', '15:38:00', '00:30:00', 'Xbox one', NULL),
+(27, '2019-07-06', '16:46:00', '00:20:00', 'Switch', NULL),
+(28, '2019-07-09', '10:00:00', '00:30:00', 'Switch', NULL),
+(29, '2019-07-09', '15:41:00', '01:00:00', 'Xbox one', NULL),
+(30, '2019-07-11', '11:46:00', '00:40:00', 'ps4', NULL),
+(31, '2019-07-11', '11:49:00', '00:30:00', 'Switch', NULL);
 
 -- --------------------------------------------------------
 
@@ -245,9 +248,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `nom`, `prenom`, `date_naissance`, `date_inscription`, `pseudo`, `carte`, `pass`, `pegi`, `statut`, `fin_inscription`, `temps`) VALUES
-(1, 'Stoklosa', 'Lilian', '1992-09-11', '2019-06-25', 'Faarged', '1', '$2y$10$/PeYrKXWAP5TKZuzlGDtbu1aC3CFCsuew3l4lkiL5rp6/IZR7oBru', 18, 'administrateur', '2019-08-25', NULL),
+(1, 'Stoklosa', 'Lilian', '1992-09-11', '2019-06-25', 'Faarged', '1', '$2y$10$/PeYrKXWAP5TKZuzlGDtbu1aC3CFCsuew3l4lkiL5rp6/IZR7oBru', 18, 'administrateur', '2029-08-25', NULL),
 (2, 'Petite', 'Personne', '2013-01-06', '2019-06-26', 'pepette', '12', '$2y$10$qEzQXaZcmbeLEAgdNWI8puv1HXIACvj6JNVQjNwXW8wzprVghS3W2', 6, 'adherent', '2020-06-26', '01:00:00'),
-(4, 'admin', 'admin', '1992-01-08', '2019-06-28', 'admin', '2', '$2y$10$5StkyTONxDnTqmc6PXDswOzJreFKgO8Og344Js9grUCzTjgA0Ccwi', 18, 'administrateur', '2020-01-01', NULL),
+(4, 'admin', 'admin', '1992-01-08', '2019-06-28', 'admin', '0000', '$2y$10$5StkyTONxDnTqmc6PXDswOzJreFKgO8Og344Js9grUCzTjgA0Ccwi', 18, 'administrateur', '2030-01-01', NULL),
 (5, 'guillaume', 'guillaume', '1990-01-01', '2019-07-02', 'guillaume', '3', '$2y$10$K7jDQZWtsJOTwZPlSdxaNupzS8WwIkyULKPZepG4HtocZjYjqLI8S', 18, 'administrateur', '2031-01-01', NULL),
 (6, 'Faarged', 'Dogrof', '1992-09-11', '2019-07-03', 'Degraaf', '7', '$2y$10$WUWC0sp9JaRP/Vhp5X0cwO032.REClCKKTP5PHSqTHJXowLDLJGh6', 18, 'adherent', '2020-06-30', '02:00:00'),
 (7, 'Faa', 'Fee', '1999-02-19', '2019-07-03', 'Faergya', '5', '$2y$10$Wn6k426us4avOs37iUj3BO2Q9BTy01kv1bjzV0TNtsQH8dPfNd//y', 18, 'adherent', '2020-07-01', '02:00:00'),
@@ -331,12 +334,12 @@ ALTER TABLE `jeux`
 -- AUTO_INCREMENT pour la table `materiel`
 --
 ALTER TABLE `materiel`
-  MODIFY `id_materiel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_materiel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id_resa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_resa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
